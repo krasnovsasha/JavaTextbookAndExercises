a668d51bd617a9018fee85650bf25e2d9fcded66
krasnovsasha <pancasancha4@gmail.com>
2018-08-09 00:49:29 +0300

    добавил метод sumOfDigitsOfNumber, который вычисляет сумму цифр двузначного числа n

--- DataTypes.java
+++ DataTypes.java
@@ -1,9 +1,13 @@
 public class DataTypes {
     public static void main(String[] args) {
         divisionWithRemainders(48, 6);
+        sumOfDigitsOfNumber(10);
 
     }
     public static void divisionWithRemainders(int q, int w){
         System.out.println(q%w);
+    }
+    public static void sumOfDigitsOfNumber(int n){
+        System.out.println(n/10 + n%10);
     }
 }
