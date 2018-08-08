41017797168bdd46271f81132313861b71182e86
krasnovsasha <pancasancha4@gmail.com>
2018-08-09 01:01:19 +0300

    написал метод roundingOfNumber, который округляет число с дробной частью до целого

--- DataTypes.java
+++ DataTypes.java
@@ -2,6 +2,7 @@ public class DataTypes {
     public static void main(String[] args) {
         divisionWithRemainders(48, 6);
         sumOfDigitsOfNumber(10);
+        roundingOfNumber(2.8);
 
     }
     public static void divisionWithRemainders(int q, int w){
@@ -9,5 +10,9 @@ public class DataTypes {
     }
     public static void sumOfDigitsOfNumber(int n){
         System.out.println(n/10 + n%10);
+    }
+    public static void roundingOfNumber(double n){
+        int a;
+        System.out.println(a = (int) (0.5 + n));
     }
 }
