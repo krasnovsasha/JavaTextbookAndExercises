e671a4c492f00c0ba0d40d1a80cf2d55061e2150
krasnovsasha <pancasancha4@gmail.com>
2018-08-09 01:37:12 +0300

    написал метод oddOrEven, который проверяет четное/нечетное число

---
+++ LogicalOperators.java
@@ -0,0 +1,11 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+    }
+    public static void oddOrEven(int n){
+        if (n%2 == 0)
+            System.out.println("Число четное");
+        else
+            System.out.println("Число нечетное");
+    }
+}
