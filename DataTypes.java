9dd46e95ca9a9baca9d0c5611c41a546922933e3
krasnovsasha <pancasancha4@gmail.com>
2018-08-09 00:42:38 +0300

    написал метод divisionWithRemainders,который вычисляет остаток от деления двух натуральных чисел

---
+++ DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        divisionWithRemainders(48, 6);
+
+    }
+    public static void divisionWithRemainders(int q, int w){
+        System.out.println(q%w);
+    }
+}
